19ec87944bf3aa1b612633639836f21b8cd3afb7
Chance Phiniezy <phiniezyc@gmail.com>
2019-02-13 05:51:33 -0500

    Add express boilerplate

--- server.js
+++ server.js
@@ -1,2 +1,10 @@
 // initial server.js file
 // Use Express for routing
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, ()=> {
+  console.log(`🌎 ==> Server now on port ${PORT}!`);
+});
