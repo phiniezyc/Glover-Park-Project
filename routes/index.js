c3c7a07408e7787de5d404d2665440299caeb459
Chance Phiniezy <phiniezyc@gmail.com>
2019-02-18 01:17:50 -0500

    Connect routes to main server file

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,8 @@
-const apiRoutes = require("./api");
+const router = require('express').Router();
+
+const apiRoutes = require('./api');
+
+router.use('/api', apiRoutes);
+
+module.exports = router;
+
--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// app.use(routing)
 // mongodb or SQL
 // Use spotify API to pull in created playlist
 
@@ -7,10 +6,14 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const routes = require('./routes');
+
 app.get('/', (req, res) => {
-	res.send('works');
+  res.send('works');
 });
 
+app.use(routes);
+
 app.listen(PORT, () => {
-	console.log(`🌎 ==> Server now on port ${PORT}!`);
+  console.log(`🌎 ==> Server now on port ${PORT}!`);
 });
