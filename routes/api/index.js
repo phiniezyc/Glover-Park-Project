12f895b3b5ae6c54e47b8ddc305e587321d7497b
Chance Phiniezy <phiniezyc@gmail.com>
2019-02-21 02:48:30 -0500

    Add instruction comment

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 const spotifyPlaylistRoutes = require('./spotifyPlaylist');
 const spotifyAuthRoutes = require('./spotifyAuth');
 
-
+// Clean this structure up.  Have 1 file for spotify routes imported here
 router.use('/spotify', spotifyPlaylistRoutes, spotifyAuthRoutes);
 
 module.exports = router;
