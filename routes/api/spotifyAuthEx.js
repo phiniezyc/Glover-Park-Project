06e9839f9052f99ec6de3641b3af58eb566d7266
Chance Phiniezy <phiniezyc@gmail.com>
2019-02-23 05:04:58 -0500

    Add demo code Working code example to make easier. Unfortunately uses a few modules and other code that doesn't conform

---
+++ routes/api/spotifyAuthEx.js
@@ -0,0 +1,128 @@
+const express = require('express'); // Express web server framework
+const request = require('request'); // "Request" library
+const cors = require('cors');
+const querystring = require('querystring');
+const cookieParser = require('cookie-parser');
+
+const client_id = 'client_id'; // Your client id
+const client_secret = 'client_secret'; // Your secret
+const redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
+
+/**
+ * Generates a random string containing numbers and letters
+ * @param  {number} length The length of the string
+ * @return {string} The generated string
+ */
+const generateRandomString = function (length) {
+  let text = '';
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+
+const stateKey = 'spotify_auth_state';
+
+const app = express();
+
+app
+  .use(express.static(`${__dirname}/public`))
+  .use(cors())
+  .use(cookieParser());
+
+app.get('/login', (req, res) => {
+  const state = generateRandomString(16);
+  res.cookie(stateKey, state);
+
+  // your application requests authorization
+  const scope = 'user-read-private user-read-email';
+  res.redirect(`https://accounts.spotify.com/authorize?${querystring.stringify({
+    response_type: 'code',
+    client_id,
+    scope,
+    redirect_uri,
+    state,
+  })}`);
+});
+
+app.get('/callback', (req, res) => {
+  // your application requests refresh and access tokens after checking the state
+  // parameter
+
+  const code = req.query.code || null;
+  const state = req.query.state || null;
+  const storedState = req.cookies
+    ? req.cookies[stateKey]
+    : null;
+
+  if (state === null || state !== storedState) {
+    res.redirect(`/#${querystring.stringify({ error: 'state_mismatch' })}`);
+  } else {
+    res.clearCookie(stateKey);
+    const authOptions = {
+      url: 'https://accounts.spotify.com/api/token',
+      form: {
+        code,
+        redirect_uri,
+        grant_type: 'authorization_code',
+      },
+      headers: {
+        Authorization: `Basic ${new Buffer(`${client_id}:${client_secret}`).toString('base64')}`,
+      },
+      json: true,
+    };
+
+    request.post(authOptions, (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        let access_token = body.access_token,
+          refresh_token = body.refresh_token;
+
+        const options = {
+          url: 'https://api.spotify.com/v1/me',
+          headers: {
+            Authorization: `Bearer ${access_token}`,
+          },
+          json: true,
+        };
+
+        // use the access token to access the Spotify Web API
+        request.get(options, (error, response, body) => {
+          console.log(body);
+        });
+
+        // we can also pass the token to the browser to make requests from there
+        res.redirect(`/#${querystring.stringify({ access_token, refresh_token })}`);
+      } else {
+        res.redirect(`/#${querystring.stringify({ error: 'invalid_token' })}`);
+      }
+    });
+  }
+});
+
+app.get('/refresh_token', (req, res) => {
+  // requesting access token from refresh token
+  const refresh_token = req.query.refresh_token;
+  const authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    headers: {
+      Authorization: `Basic ${new Buffer(`${client_id}:${client_secret}`).toString('base64')}`,
+    },
+    form: {
+      grant_type: 'refresh_token',
+      refresh_token,
+    },
+    json: true,
+  };
+
+  request.post(authOptions, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      const access_token = body.access_token;
+      res.send({ access_token });
+    }
+  });
+});
+
+console.log('Listening on 8888');
+app.listen(8888);
