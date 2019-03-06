7ee5dc9f54e22a714acf1a9edbb083ffd62c15d6
Chance Phiniezy <phiniezyc@gmail.com>
2019-03-06 02:35:56 -0500

    Add helmet headers module

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const helmet = require('helmet');
 
 
 const app = express();
@@ -9,6 +10,7 @@ const PORT = process.env.PORT || 3001;
 const routes = require('./routes');
 
 app
+  .use(helmet())
   .use(express.static(`${__dirname}/public`))
   .use(cors())
   .use(cookieParser());
