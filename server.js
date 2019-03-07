dbdec32b6fa1df1f84db5a6abc84bfc1949db0d5
Chance Phiniezy <phiniezyc@gmail.com>
2019-03-07 00:19:07 -0500

    Add morgan logger

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
+const morgan = require('morgan');
 
 
 const app = express();
@@ -11,6 +12,7 @@ const routes = require('./routes');
 
 app
   .use(helmet())
+  .use(morgan)
   .use(express.static(`${__dirname}/public`))
   .use(cors())
   .use(cookieParser());
