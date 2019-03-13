6e8ccffc381e06b3af886514730e7b6b7e9e4377
Chance Phiniezy <phiniezyc@gmail.com>
2019-03-13 00:15:51 -0400

    Format code

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import IndexPage from './pages/index.js';
 import SpotifyLoggedIn from './pages/spotifyLoggedIn';
 
