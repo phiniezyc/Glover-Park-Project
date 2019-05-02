73c60551766b63a6bcd28efde1eca03f1c68cc3f
Chance Phiniezy <phiniezyc@gmail.com>
2019-05-02 02:16:29 -0400

    Abstract out playlists into Playlist component

--- client/src/components/Playlist.js
+++ client/src/components/Playlist.js
@@ -1,8 +1,17 @@
 import React from 'react';
 
 const Playlists = (props) => {
+
+
+    const playlists = props.playlists ? props.playlists.map(playlist =>{
+         return <h4 key={playlist.id}>{playlist.name}</h4>
+         }) : "Loading...";
     return (
-        <h3>You have SpotiyPlaylists: {props.playlists}</h3>
+        <div>
+            {playlists}
+        </div>
+
+
     )
 }
 
--- client/src/pages/spotifyLoggedIn.js
+++ client/src/pages/spotifyLoggedIn.js
@@ -17,7 +17,7 @@ class spotifyLoggedIn extends Component {
 
     this.state = {
       access_token: params.access_token,
-      spotifyPlaylists: [] 
+      spotifyPlaylists: []
     };
   }
   // FIXME: USE REACT URL PARAMS INSTEAD OF GETHASH
@@ -60,30 +60,14 @@ class spotifyLoggedIn extends Component {
   }
 
   render() {
-    const playlists = this.state.spotifyPlaylists.items ? this.state.spotifyPlaylists.items.length : 0;
+    
     return (
       <React.Fragment>
           <header>
             <h2>We got your spotify</h2>
           </header>
 
-
-
-          <Playlist playlists={playlists}/>
-
-            {this.state.spotifyPlaylists.items ? (
-              <h2>{this.state.spotifyPlaylists.items.length}</h2>
-            ) : (
-              0
-            )}
-
-            {this.state.spotifyPlaylists.items
-              ? this.state.spotifyPlaylists.items.map(playlist => {
-                  return <h4 key={playlist.id}>{playlist.name}</h4>;
-                })
-              : 0}
-
-
+          <Playlist playlists={this.state.spotifyPlaylists.items} />
 
       </React.Fragment>
     );
