c81766c7a1af080bf552203416cd18f2afc55f3d
Chance Phiniezy <phiniezyc@gmail.com>
2019-05-02 02:46:40 -0400

    Add style variable for inline style to playlists

--- client/src/components/Playlist.js
+++ client/src/components/Playlist.js
@@ -1,13 +1,22 @@
 import React from 'react';
 
 const Playlists = (props) => {
-
-
+    {//FIXME: Need to remove this styling.  Only for quick dev}
+    const divStyle = {
+        color: 'blue',
+        height: 200,
+        width: 200,
+        display: 'block',
+        border: '2px solid green',
+        margin: 10,
+        };
     const playlists = props.playlists ? props.playlists.map(playlist =>{
-         return <h4 key={playlist.id}>{playlist.name}</h4>
+         return <h4 style={divStyle} key={playlist.id}>{playlist.name}</h4>
          }) : "Loading...";
+
+
     return (
-        <div>
+        <div >
             {playlists}
         </div>
 
