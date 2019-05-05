5b7cea40ecff30506b323c1f8b21bf16af2ae2e7
Chance Phiniezy <phiniezyc@gmail.com>
2019-05-05 00:33:14 -0400

    Format Ternary

--- client/src/components/Playlist.js
+++ client/src/components/Playlist.js
@@ -1,47 +1,47 @@
 import React from 'react';
 
-const Playlists = (props) => {
-    //FIXME: Need to remove this styling.  Only for quick dev;
-    const playlistDiv = {
-        color: 'blue',
-        minHeight: 300,
-        width: '33%',
-        display: 'flex',
-        border: '2px solid green',
-        margin: 10,
-        flex: 1,
-        textAlign: 'center'
-        };
+const Playlists = props => {
+  //FIXME: Need to remove this styling.  Only for quick dev;
+  const playlistDiv = {
+    color: 'blue',
+    minHeight: 300,
+    width: '33%',
+    display: 'flex',
+    border: '2px solid green',
+    margin: 10,
+    flex: 1,
+    textAlign: 'center'
+  };
 
-    const imgStyle = {
-        width:200,
-        height:200,
-        // display: 'block'
-    }
-    const playlists = props.playlists ? props.playlists.map(playlist =>{
-         return (
-             <div style={playlistDiv}>
-                <h4  key={playlist.id}>Playlist: </h4>
-                {playlist.name}
-                <h4>Owner:</h4>
-                {playlist.owner.display_name}
-                <br></br>
-                <img style={imgStyle} src={playlist.images[0].url} />
-            </div>
-            )
-         }) : "Loading...";
+  const imgStyle = {
+    width: 200,
+    height: 200
+    // display: 'block'
+  };
+  const playlists = props.playlists
+    ? props.playlists.map(playlist => {
+        return (
+          <div style={playlistDiv}>
+            <h4 key={playlist.id}>Playlist: </h4>
+            {playlist.name}
+            <h4>Owner:</h4>
+            {playlist.owner.display_name}
+            <br />
+            <img style={imgStyle} src={playlist.images[0].url} />
+          </div>
+        );
+     })
+    : 'Loading...';
 
-    const divStyle = {
-        display: 'flex',
-        flexWrap: 'wrap',
-        justifyContent: 'center'
-    }
-    return (
-        // FIXME: CHANGE TO SEMANTIC HTML 5
-        <div style={divStyle}>
-        {playlists}
-        </div>
-    )
-}
+  const divStyle = {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'center'
+  };
+  return (
+    // FIXME: CHANGE TO SEMANTIC HTML 5
+    <div style={divStyle}>{playlists}</div>
+  );
+};
 
 export default Playlists;
