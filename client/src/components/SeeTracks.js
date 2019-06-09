9ec0618f77d1931fc89ed1bdc57dc631d525bd93
Chance Phiniezy <phiniezyc@gmail.com>
2019-06-09 01:30:12 -0400

    Add Boilerplate getTracks Function Not working yet

--- client/src/components/SeeTracks.js
+++ client/src/components/SeeTracks.js
@@ -4,8 +4,30 @@ class SeeTracks extends Component {
   constructor() {
     super();
   }
-  render() {
 
+  getUserTracks = () => {
+    const options = {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${this.state.access_token}`,
+        'Content-Type': 'application/json'
+      }
+    };
+    fetch('https://api.spotify.com/v1/me/playlists', options)
+      .then(response => response.json())
+      .then(spotifyPlaylists =>
+        this.setState({
+          spotifyPlaylists
+        })
+      )
+      .catch(error => console.log(error.message)); // FIXME: Don't want to log this to users
+  };
+
+  componentDidMount() {
+    this.getUserTracks();
+  }
+
+  render() {
     return (
       <button>See Songs</button>
       )
