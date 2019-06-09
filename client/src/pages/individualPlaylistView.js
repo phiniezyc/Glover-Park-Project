48c86c25e2de44bb2216e96501bc124971e13aec
Chance Phiniezy <phiniezyc@gmail.com>
2019-06-09 00:09:52 -0400

    Change component to individualPlaylistView

--- client/src/pages/individualPlaylistView.js
+++ client/src/pages/individualPlaylistView.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-class individualPlaylist extends Component {
+class individualPlaylistView extends Component {
   constructor(props) {
     super(props);
 
