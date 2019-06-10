4f63e56b75080e8aac26898d5f09d41a871f1d5d
Chance Phiniezy <phiniezyc@gmail.com>
2019-06-10 02:38:34 -0400

    Add individualPlaylistView Route to React Router

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import IndexPage from './pages/index.js';
 import SpotifyLoggedIn from './pages/spotifyLoggedIn.js';
+import individualPlaylistView from './pages/individualPlaylistView.js';
 
 import './App.css';
 
@@ -14,6 +15,7 @@ const App = () => (
         <Switch>
           <Route exact path="/" component={IndexPage} />
           <Route exact path="/spotifyLoggedIn" component={SpotifyLoggedIn} />
+          <Route exact path="/individualPlaylist" component={individualPlaylistView} />
         </Switch>
       </div>
     </BrowserRouter>
