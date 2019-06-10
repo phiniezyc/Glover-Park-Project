257d60c0a55ad3a8c553ab3812f58ede03c21ed0
Chance Phiniezy <phiniezyc@gmail.com>
2019-06-10 02:34:42 -0400

    Refactor Base App Component to Functional Component

--- client/src/App.js
+++ client/src/App.js
@@ -2,26 +2,23 @@ import React, { Component } from 'react';
 
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import IndexPage from './pages/index.js';
-import SpotifyLoggedIn from './pages/spotifyLoggedIn';
+import SpotifyLoggedIn from './pages/spotifyLoggedIn.js';
 
 import './App.css';
 
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <BrowserRouter>
-          <div>
-            <Switch>
-              <Route exact path='/' component={IndexPage}/>
-              <Route exact path='/spotifyLoggedIn' component={SpotifyLoggedIn}/>
-            </Switch>
-          </div>
-        </BrowserRouter>
+const App = () => (
+  <div className="App">
+    <BrowserRouter>
+      <div>
+        <Switch>
+          <Route exact path="/" component={IndexPage} />
+          <Route exact path="/spotifyLoggedIn" component={SpotifyLoggedIn} />
+        </Switch>
       </div>
-    );
-  }
-}
+    </BrowserRouter>
+  </div>
+);
+
 
 export default App;
