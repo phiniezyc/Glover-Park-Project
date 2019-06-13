2ebcea0d624f9c61a448083f705947a28daeaab1
Chance Phiniezy <phiniezyc@gmail.com>
2019-06-13 04:25:08 -0400

    Add provider component and store to app

--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,4 @@
+import { createStore } from 'redux';
 import { Provider, connect } from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -7,7 +8,9 @@ import App from './App';
 
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer);
+
+ReactDOM.render(<Provider store={store}> <App /> </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
