cd1da2151127579212b159e08e6da9598633c265
Chance Phiniezy <phiniezyc@gmail.com>
2019-06-13 03:31:24 -0400

    Add initial code for reducers

---
+++ client/src/reducers/index.js
@@ -0,0 +1,16 @@
+const initialState = {
+  playlistTracks: [],
+};
+
+
+// eslint-disable-next-line import/prefer-default-export
+export const getPlaylistTracks = (state = initialState, action = {}) => {
+  switch (action.type) {
+    case 'SET_PLAYLIST_TRACKS':
+      return { ...state, playlistTracks: action.payload };
+
+    default:
+      return state;
+  }
+};
+
