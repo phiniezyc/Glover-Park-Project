2d0148019c6160f9828b9fce712b1fc4dadb2e41
Chance Phiniezy <phiniezyc@gmail.com>
2019-06-19 02:18:35 -0400

    Remove eslint error for undeclared global object add env pluging for browser in eslint config

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,10 +1,32 @@
+/* eslint-disable camelcase */
 import { SET_PLAYLIST_TRACKS } from '../constants/constants';
 
-const setPlaylistTracks = (tracks) => {
-  return {
-    type: SET_PLAYLIST_TRACKS,
-    payload: tracks,
+const getPlaylistTracks = (props) => {
+  // eslint-disable-next-line camelcase
+  const access_token = sessionStorage.spotifyToken;
+  // eslint-disable-next-line camelcase
+  const playlist_id = this.state.selectedPlaylistId; // FIXME: not using state here because redux
+
+  const options = {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${access_token}`,
+      'Content-Type': 'application/json',
+    },
   };
+
+  fetch(`https://api.spotify.com/v1/playlists/${playlist_id}/tracks`, options)
+    .then(response => response.json())
+    .then(playlistTracks =>
+      this.setState({
+        playlistTracks,
+      }))
+    .catch(error => console.log(error.message));
 };
+
+const setPlaylistTracks = tracks => ({
+  type: SET_PLAYLIST_TRACKS,
+  payload: tracks,
+});
 
 export default { setPlaylistTracks };
