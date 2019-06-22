f85cdef62b828d0aa9ecc18ec1a12216a2bfa957
Chance Phiniezy <phiniezyc@gmail.com>
2019-06-21 23:09:35 -0400

    Remove comments and unused imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,10 @@
-// import { createStore, applyMiddleware } from 'redux';
 import React from 'react';
-// import thunk from 'redux-thunk';
-
-// import rootReducer from './reducers';
 
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import IndexPage from './pages/index';
 import spotifyLoggedIn from './pages/spotifyLoggedIn';
 
 import './App.css';
-
-// const store = createStore(
-//   rootReducer,
-//   applyMiddleware(thunk),
-// );
-
 
 const App = () => (
   <div className="App">
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,6 @@
 
 import { combineReducers } from 'redux';
-// import thunk from 'redux-thunk';
-// import rootReducer from './reducers/index';
+
 import PlayListTracksReducer from './playlistsReducer';
 
 const rootReducer = combineReducers({
