75277a4ca098bff12c43ea58feab667741862477
Chance Phiniezy <phiniezyc@gmail.com>
2019-06-25 23:13:06 -0400

    convert See Tracks Button to a functional component no longer using local react state

--- client/src/components/SeeTracksButton.js
+++ client/src/components/SeeTracksButton.js
@@ -1,45 +1,41 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import { connect } from 'react-redux';
 import { fetchPlaylistTracks } from '../actions/index';
 
 // FIXME: Make a presentational component (functional) since using redux for state management.
-class SeeTracksButton extends Component {
-
-  render() {
-    // V4 of React-Router Redirect component instead of directly interacting w/ this.props.history
-
-    const buttonDivStyle = {
-      flex: '100%'
-    };
-    return (
-        <div style={buttonDivStyle}>
-          {/* // FIXME: Need to get playlist id in, now hardcoded */}
-          <button onClick={() => this.props.fetchPlaylistTracks(this.props.playlistId)}>Redux Tracks</button>
-          { // FIXME: REMOVE AFTER DEV
-            (this.props.playlistTracks) ? <h2>{this.props.playlistTracks.length}</h2> : "loading"
+const SeeTracksButton = (props) => {
+  const buttonDivStyle = {
+    flex: '100%',
+  };
+  return (
+    <div style={buttonDivStyle}>
+      {/* // FIXME: Need to get playlist id in, now hardcoded */}
+      <button onClick={() => props.fetchPlaylistTracks(props.playlistId)}>Redux Tracks</button>
+      { // FIXME: REMOVE AFTER DEV
+            (props.playlistTracks) ? <h2>{props.playlistTracks.length}</h2> : 'loading'
           }
-        </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
 
 // This function is used to convert redux global state to desired props.
-function mapStateToProps(state, ) {
+function mapStateToProps(state) {
   // console.log("yo", ownProps.playlistId);
   // `state` variable contains whole redux state.
   return {
-    playlistTracks: state.playlistTracks.tracks
+    playlistTracks: state.playlistTracks.tracks,
   };
-  }
+}
 
-  // This function is used to provide callbacks to container component.
+// This function is used to provide callbacks to container component.
 function mapDispatchToProps(dispatch) {
   return {
     // This function will be available in component as `this.props.fetchTodos`
     fetchPlaylistTracks: (playlistId) => {
       dispatch(fetchPlaylistTracks(playlistId));
-    }
+    },
   };
 }
 
