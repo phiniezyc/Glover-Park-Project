9118437b93aca5984e9bc007abd0e6bf26243fa4
Chance Phiniezy <phiniezyc@gmail.com>
2019-09-18 19:16:17 -0400

    Add fetch reducer boilerplate

--- client/src/reducers/reducers.test.js
+++ client/src/reducers/reducers.test.js
@@ -1,7 +1,13 @@
+
 import {
   FETCH_TRACKS,
   DELETE_TRACKS,
 } from '../constants';
 
-import * as reducers from './index';
+import rootReducer from './index';
 
+describe('spotifyTracks', () => {
+  it('should return initial state', () => {
+    expect(rootReducer.playlistTracks.PlaylistTracksReducer(undefined, {})).toEqual({ tracks: [] });
+  });
+});
