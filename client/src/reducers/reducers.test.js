f558ec4ef8939c185be1e92593e0e23fdf05c6b5
Chance Phiniezy <phiniezyc@gmail.com>
2019-09-20 18:02:58 -0400

    Clean up Initial State

--- client/src/reducers/reducers.test.js
+++ client/src/reducers/reducers.test.js
@@ -1,20 +1,21 @@
 
-import {
-  FETCH_TRACKS,
-  DELETE_TRACKS,
-} from '../constants';
+// import {
+//   FETCH_TRACKS,
+//   DELETE_TRACKS,
+// } from '../constants';
 
 import playlistTracksReducer from './playlistsReducer';
 
 describe('fetch tracks', () => {
+  const INITIAL_STATE = {
+    tracks: [],
+    isFetching: false,
+    isDeleting: false,
+    tracksToDelete: [],
+    tracksToDeleteIDs: [],
+    error: undefined,
+  };
   it('should return initial state', () => {
-    expect(playlistTracksReducer(undefined, {})).toEqual({
-      tracks: [],
-      isFetching: false,
-      isDeleting: false,
-      tracksToDelete: [],
-      tracksToDeleteIDs: [],
-      error: undefined,
-    });
+    expect(playlistTracksReducer(undefined, {})).toEqual(INITIAL_STATE);
   });
 });
